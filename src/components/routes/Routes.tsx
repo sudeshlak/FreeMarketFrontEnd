19e4abcf57bc5e6c65e0ec20a462e03350a8982c
Sudeshlak <swelikotuwa@gmail.com>
2026-05-19 09:21:09 +0530

    added lazy loading for code splitting

--- src/components/routes/Routes.tsx
+++ src/components/routes/Routes.tsx
@@ -1,33 +1,42 @@
-import React from "react";
-import Checkout from "../checkout/Checkout";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
-import Register from "../register/Register";
-import Login from "../Login/Login";
-import AdminPanel from "../adminPanel/AdminPanel"
+import React, { Suspense } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ShoppingApp from "../../views/ShoppingApp";
-import UserAccount from "../UserAccount/UserAccount";
-import FAQ from "../FAQ/FAQ";
-import AboutUs from "../aboutUs/AboutUs";
-import ContactUs from "../ContactUs/ContactUs";
-import DeliveryArea from "../deliveryArea/DeliveryArea";
+import { Spinner } from "react-bootstrap";
+const FAQ = React.lazy(() => import("../FAQ/FAQ"));
+const AboutUs = React.lazy(() => import("../aboutUs/AboutUs"));
+const ContactUs = React.lazy(() => import("../ContactUs/ContactUs"));
+const AdminPanel = React.lazy(() => import("../adminPanel/AdminPanel"));
+const DeliveryArea = React.lazy(() => import("../deliveryArea/DeliveryArea"));
+const Checkout = React.lazy(() => import("../checkout/Checkout"));
+const UserAccount = React.lazy(() => import("../UserAccount/UserAccount"));
+const Register = React.lazy(() => import("../register/Register"));
+const Login = React.lazy(() => import("../Login/Login"));
 
 const Routing = () => {
   return (
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={ShoppingApp}/>
-            <Route path="/checkout" component={Checkout}/>
-            <Route path="/register" component={Register}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/admin" component={AdminPanel}/>
-            <Route path="/FAQ" component={FAQ} />
-            <Route path="/about_us" component={AboutUs} />
-            <Route path="/contact_us" component={ContactUs} />
-            <Route path="/userAccount" component={UserAccount}/>
-            <Route path="/deliveryArea" component={DeliveryArea}/>
-          </Switch>
-        </BrowserRouter>
+    <BrowserRouter>
+      <Suspense
+        fallback={
+          <div className="text-center py-5">
+            <Spinner animation="border" />
+          </div>
+        }
+      >
+        <Switch>
+          <Route exact path="/" component={ShoppingApp} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/admin" component={AdminPanel} />
+          <Route path="/FAQ" component={FAQ} />
+          <Route path="/about_us" component={AboutUs} />
+          <Route path="/contact_us" component={ContactUs} />
+          <Route path="/userAccount" component={UserAccount} />
+          <Route path="/deliveryArea" component={DeliveryArea} />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
   );
-}
+};
 
 export default Routing;
